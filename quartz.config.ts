dfede4bf9635418bb4d6b8fbf2cd3b52e5a97fc7
Tristan1118 <tristan.ebert@googlemail.com>
2026-03-29 19:11:21 +0100

    Adjust config

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "tristan1118.github.io/notes-public",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
